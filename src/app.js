b45a4ff88f65862210a18dc2fcd2ef3d1d1937a3
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-20 13:47:38 -0500

    added more html

--- src/app.js
+++ src/app.js
@@ -12,11 +12,19 @@ class App extends React.Component {
                     </nav>
                 </header>
                 <section className="notes">
-                
+                    <div className="noteCard">
+                    <i className="fa fa-edit"></i>
+                    <i className="fa fa-lines"></i>
+                    <h4>Test Note</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fuga aliquam ipsa. Repellat, tempore et minus iste consequatur quia iusto laboriosam aspernatur at ipsam. Eaque saepe iusto tempora neque vel.</p>
+                    </div>
                 </section>
                 <aside className="sidebar">
                     <form>
-                    <h3>Add New Note</h3>
+                        <h3>Add New Note</h3>
+                        <div className="close-btn">
+                            <i className="fa fa-times"></i>
+                        </div>
                         <label htmlFor="note-title">Title</label>
                         <input type="text" name="note-title"/>
                         <label htmlFor="note-text">Text</label>
