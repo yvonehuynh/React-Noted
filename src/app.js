acc7860d3b00a74db372141441b9ad6b1ae832a4
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-20 21:44:35 -0500

    added functionality

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NoteCard from "./notesCard";
 
 class App extends React.Component {
     constructor(){
         super();
+        this.state={
+            notes: []
+        }
     this.showSidebar = this.showSidebar.bind(this);
+    this.addNote = this.addNote.bind(this);
     }
     showSidebar(e) {
         e.preventDefault();
-        this.sideBar.classList.add("show");
+        this.sideBar.classList.toggle("show");
+    }
+    addNote(e){
+        e.preventDefault();
+        const note = {
+            title: this.noteTtitle.value,
+            text: this.noteText.value
+        }
+        const newNotes = Array.from(this.state.notes);
+        newNotes.push(note);
+        this.setState({
+            notes: newNotes
+        })
+        this.noteText.value = "",
+        this.noteText.value ="",
+        this.showSidebar(e);
     }
     render(){
         return (
@@ -20,23 +40,22 @@ class App extends React.Component {
                     </nav>
                 </header>
                 <section className="notes">
-                    <div className="noteCard">
-                    <i className="fa fa-edit"></i>
-                    <i className="fa fa-times"></i>
-                    <h4>Test Note</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fuga aliquam ipsa. Repellat, tempore et minus iste consequatur quia iusto laboriosam aspernatur at ipsam. Eaque saepe iusto tempora neque vel.</p>
-                    </div>
+                    {this.state.notes.map((note, i) => {
+                        return (
+                            <NoteCard note={note} key={`note-${i}`}/>
+                        )
+                    }).reverse()}
                 </section>
                 <aside className="sidebar" ref={ref=>this.sideBar = ref}>
-                    <form>
+                    <form onSubmit={this.addNote}>
                         <h3>Add New Note</h3>
-                        <div className="close-btn">
+                        <div className="close-btn" onClick={this.showSidebar}>
                             <i className="fa fa-times"></i>
                         </div>
                         <label htmlFor="note-title">Title</label>
-                        <input type="text" name="note-title"/>
+                        <input type="text" name="note-title" ref={ref => this.noteTtitle = ref}/>
                         <label htmlFor="note-text">Text</label>
-                        <textarea name="note-text" ></textarea>
+                        <textarea name="note-text" ref={ref =>this.noteText = ref}></textarea>
                         <input type="submit" value="Add New Note"/>
                     </form>
                 </aside>
---
+++ src/notesCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function(props){
+    return (
+        <div className="noteCard">
+            <i className="fa fa-edit"></i>
+            <i className="fa fa-times"></i>
+            <h4>{props.note.titile}</h4>
+            <p>{props.note.text}</p>
+        </div>
+    )
+}
