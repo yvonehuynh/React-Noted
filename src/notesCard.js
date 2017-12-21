f001a4bf5d91b3b076c96eed7c5a5e67679d7b94
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-21 10:58:45 -0500

    added editing functionality

--- src/notesCard.js
+++ src/notesCard.js
@@ -7,26 +7,47 @@ export default class NoteCard extends React.Component{
             editing: false,
             note: {}
         }
+        this.save = this.save.bind(this);
     }
-    componentDidMount(){
-        this.setState({
-            note: this.props.note
-        })
+    save(e){
+        e.preventDefault();
+         const dbRef = firebase.database().ref(this.props.note.key);
+         dbRef.update({
+             title: this.noteTitle.value,
+             text: this.noteText.value
+         });
+
+         this.setState({
+             editing: false
+         });
     }
-    render(){
+        render(){
         let editingTemp = (
-            <div>
-                <h4>{this.state.note.title}</h4>
-                <p>{this.state.note.text}</p>
-            </div>
+            <span>
+                <h4>{this.props.note.title}</h4>
+                <p>{this.props.note.text}</p>
+            </span>
         )
+            if (this.state.editing) {
+            editingTemp = (
+                <form onSubmit={this.save}>
+                    <div>
+                        <input type="text" defaultValue={this.props.note.title} name="title" ref={ref => this.noteTitle = ref}/>
+                    </div>
+                    <div>
+                        <input type="text" defaultValue={this.props.note.text} name="text" ref ={ref => this.noteText = ref}/>
+                    </div>
+                    <input type="submit" value="Done editing"/>
+                </form>
+            )
+        }
         return (
             <div className="noteCard">
                 <i className="fa fa-edit" onClick={()=>{
                     this.setState({editing: true})
                 }}></i>
                 <i className="fa fa-times" onClick={(()=>{
-                    props.removeNote(this.state.note.key)
+                    this.props.removeNote(this.props.note.key)
                 })}></i>
                 {editingTemp}
             </div>
